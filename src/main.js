369c785cbdfc1530f8191146fe69e269c98cd2cf
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-02-18 06:05:39 +0000

    Basic subscriptions are working (again)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import {default as koa} from 'koa'
 import {default as bodyParser} from 'koa-bodyparser'
+import {default as logger} from 'koa-logger'
 
 import {default as heartbeat} from './heartbeat'
 import {default as route} from './routes'
@@ -9,18 +10,8 @@ const NAME = 'notifications'
 const VERSION = '0.0.1'
 const PORT = process.env.PORT || 80
 
-// Load required environment variables
-function loadEnv (envName) {
-  const env = process.env[envName]
-  if (!env) throw new Error('Failed to load ENV variable: ' + envName)
-}
-
-// The SDK loads env variables automatically, so we just need to make sure
-// they are actually loaded
-loadEnv('AWS_ACCESS_KEY_ID')
-loadEnv('AWS_SECRET_ACCESS_KEY')
-
 koa()
+	.use(logger())
   .use(bodyParser())
   .use(route.routes())
   .listen(PORT)
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import {default as postmark} from 'postmark'
 import {default as redis} from 'node-redis'
 import {default as jade} from 'jade'
 
+import {loadEnv} from './utils'
+
 // Configure postmark
 const postClient = Promise.promisifyAll(new postmark.Client(loadEnv('POSTMARK_API_KEY')))
 
@@ -16,23 +18,47 @@ const rc = redis.createClient()
 export default new Router()
   // Makes a list of subscribers,
   .post('/list/:name', function *(next) {
+    _.map(this.request.body.emails, email => {
+      rc.sadd(`list:${this.params.name}`, email)
+    })
+    this.body = 'Created new list'
   })
-  // Adds a new jade template that can be sent to a list of subscribesr
+  // Gets all of the subscribers in a list
+  .get('/list/:name', function *(next) {
+    const members = yield rc.smembersAsync(`list:${this.params.name}`)
+    // Gets a BUFFER, not a string
+    this.body = _.map(members, member => member.toString())
+  })
+  // Adds a new jade template that can be sent to a list of subscribers
   .post('/template/:name', function *(next) {
+    rc.set(`template:${this.params.name}`, this.params.name)
+    this.body = 'Added new template'
   })
   // Select a given template and send the message to the given list
   // Takes a json object with locals for the jade template
   .post('/template/:name/:listName/send', function *(next) {
+    // Load up the template
+    const template = yield rc.getAsync(`template:${this.params.name}`)
+    // Load up the emails to send
+    const emails = yield rc.smembersAsync(`list:${this.params.name}`)
+
+    const locals = this.request.body.locals
+
+    // Compile the message to send
+    const generator = jade.compile(template)
+    const message = generator(locals)
+    yield batchSend(emails, message)
+    this.body = 'Successfully emailed everyone in the list'
   })
 
 // Send the list of emails the given HTML document
 function batchSend (emails, msg) {
   const messages = _.map(emails, email => {
     return postClient.sendEmail({
-      "From": "caleb@thorsteinson.io",
-      "To": email,
-      "Subject": "Test",
-      "HtmlBody": msg,
+      'From': 'caleb@thorsteinson.io',
+      'To': email,
+      'Subject': 'Test',
+      'HtmlBody': msg
     })
   })
   return Promise.all(messages)
